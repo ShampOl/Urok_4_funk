a3788d7b626758bb0fa370c953043174c3d9d0b0
Olya <lolcap202@gmail.com>
2023-11-27 19:53:06 +0600

    add zadanie2

---
+++ zadan2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int arraySize = 8; 
+
+        Random random = new Random();
+
+        int[] numbersArray = new int[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            numbersArray[i] = random.Next(100, 1000);
+        }
+
+        Console.WriteLine("Сгенерированный рандомно массив:");
+        foreach (int number in numbersArray)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        int evenCount = CountEvenNumbers(numbersArray);
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    }
+
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+
+        foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
